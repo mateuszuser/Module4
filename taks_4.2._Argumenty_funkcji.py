8a75c55151894a5470ab94fb079cf8fa5901d395
mateuszuser <ciomcia.mateusz@gmail.com>
2022-05-11 20:13:14 +0200

    task4_2

---
+++ taks_4.2._Argumenty_funkcji.py
@@ -0,0 +1,10 @@
+def check_palidrom(palidrom):
+    if palidrom[-1::-1] == palidrom[0: len(palidrom)]:
+        print(f"The word {palidrom} is palidrom")
+    else:
+        print(f"The word {palidrom} is not palidrom")
+      
+check_palidrom("potop")
+check_palidrom("kajak")
+check_palidrom("python")
+check_palidrom("oko")
